02f546249f73efdee9200725773a94d644c2828f
sanipidr <sanipidr@mail.uc.edu>
2024-02-19 15:19:41 -0500

    input validation done

--- labs/lab2/echo.php
+++ labs/lab2/echo.php
@@ -1,3 +1,7 @@
 <?php 
       echo $_REQUEST["data"];
-?>
+      if(!isset($_REQUEST["data"])){
+            die("{\"error\":\"Please provide 'data' field\"}");
+      }
+      echo htmlentities($_REQUEST['data']);
+?> 
