52ee1e8f0b2c459dd0e5f141bd587d5de3849ea0
sanipidr <sanipidr@mail.uc.edu>
2024-03-03 19:27:14 -0500

    lab3.

--- labs/lab3/index.php
+++ labs/lab3/index.php
@@ -1,6 +1,9 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 	session_start();    
-	if (checklogin($_POST["username"],$_POST["password"])) {
+	if (checklogin_mysql($_POST["username"],$_POST["password"])) {
 ?>
 	<h2> Welcome <?php echo $_POST['username']; ?> !</h2>
 <?php		
@@ -9,18 +12,24 @@
 		die();
 	}
 	function checklogin_mysql($username, $password) {
-		$mysqli = new mysqli('localhost','sanipidr' /*Database username*/, 'pa$$w0rd' /*Database password*/, 'waph' /*Database name*/);
+		$mysqli = new mysqli('localhost','sanipidr' /*Database username*/, 'Pa$$w0rd' /*Database password*/, 'waph' /*Database name*/);
 		if($mysqli->connect_errno){
 			printf("Database connection failed: %s\n", $mysqli->connect_error);
 			exit();
   	    }
-  	    $sql = "SELECT * FROM users WHERE username='" . $username . "' ";
-	    $sql = $sql . " AND password = md5('" . $password . "')";
-	    //echo "DEBUG>sql = $sql"; //return TRUE;
-	    $result = $mysqli->query($sql);
+  	    $prepared_sql = "SELECT * FROM users WHERE username=? " . " AND password=md5(?);";
+	    $stmt = $mysqli->prepare($prepared_sql);
+	    $stmt->bind_param("ss", $username,$password);
+	    $stmt->execute();
+	    if($stmt->error){
+            echo "Error: ".$stmt->error;
+        exit();
+        }
+	    $result=$stmt->get_result();
 	    if($result->num_rows ==1)
 	        return TRUE;
 	    return False;
+
 	}
 ?>
 
